ffe09c576b43ab0b5f468f9933387d128ac759ec
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-31 22:46:57 +0700

    feat: logging in morgan

--- src/middleware/morgan-middleware.js
+++ src/middleware/morgan-middleware.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import dayjs from 'dayjs';
 import morgan from 'morgan';
+import { logger } from '../application/logging.js';
 
 const customFormat = (tokens, req, res) => {
   const methodColor = chalk.green;
@@ -8,14 +9,38 @@ const customFormat = (tokens, req, res) => {
   const statusColor = chalk.red;
   const responseTimeColor = chalk.magenta;
 
-  return [
-    `Time: ${chalk.blue(dayjs(tokens.date(req, res, 'iso')).format('HH:mm:ss DD/MM/YY'))}`,
-    `Method: ${methodColor(tokens.method(req, res))}`,
-    `URL: ${urlColor(tokens.url(req, res))}`,
-    `Status: ${statusColor(tokens.status(req, res))}`,
-    `Response Time: ${responseTimeColor(tokens['response-time'](req, res))} ms`,
-    `Content Length: ${chalk.yellow(tokens.res(req, res, 'content-length') || 'unknown')}`,
+  const statusCode = tokens.status(req, res);
+  const time = dayjs(tokens.date(req, res, 'iso')).format('HH:mm:ss DD/MM/YY');
+  const method = tokens.method(req, res);
+  const url = tokens.url(req, res);
+  const responseTime = tokens['response-time'](req, res);
+  const contentLength = tokens.res(req, res, 'content-length') || 'unknown';
+
+  const logMessage = {
+    code: statusCode,
+    time,
+    method,
+    url,
+    RT: responseTime,
+    CL: contentLength,
+  };
+
+  if (statusCode >= 200 && statusCode < 300) {
+    logger.traceGood(logMessage);
+  } else if (statusCode >= 300) {
+    logger.traceFail(logMessage);
+  }
+
+  const message = [
+    `Time: ${chalk.blue(time)}`,
+    `Method: ${methodColor(method)}`,
+    `URL: ${urlColor(url)}`,
+    `Status: ${statusColor(statusCode)}`,
+    `RT: ${responseTimeColor(responseTime)} ms`,
+    `CL: ${chalk.yellow(contentLength)}`,
   ].join(' | ');
+
+  return message;
 };
 
 export default morgan(customFormat);
