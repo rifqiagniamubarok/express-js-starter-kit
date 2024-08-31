aef4b749d94627f8c64ab83c51cb8a5e9c47f072
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-31 21:51:38 +0700

    feat: logging with color

--- src/application/logging.js
+++ src/application/logging.js
@@ -1,7 +1,28 @@
 import winston from 'winston';
 
+const levelColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+};
+
+const colorizeFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.printf(({ level, message, ...rest }) => {
+    const json = JSON.stringify({ message, ...rest });
+    return `${level}: ${json}`;
+  })
+);
+
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  transports: [new winston.transports.Console({})],
+  transports: [
+    new winston.transports.Console({
+      format: colorizeFormat,
+    }),
+  ],
 });
+
+winston.addColors(levelColors);
