f96f705d177ae119f42d6b55e9f7ea9e0f8e3b74
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-31 22:47:25 +0700

    feat: add traceFail n traceGood

--- src/application/logging.js
+++ src/application/logging.js
@@ -5,6 +5,19 @@ const levelColors = {
   warn: 'yellow',
   info: 'green',
   debug: 'blue',
+  traceFail: 'red',
+  traceGood: 'green',
+  trace: 'blue',
+};
+
+const logLevels = {
+  error: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  traceFail: 5,
+  traceGood: 6,
+  trace: 7,
 };
 
 const colorizeFormat = winston.format.combine(
@@ -16,7 +29,8 @@ const colorizeFormat = winston.format.combine(
 );
 
 export const logger = winston.createLogger({
-  level: 'info',
+  levels: logLevels,
+  level: process.env.LOG_LEVEL || 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.Console({
