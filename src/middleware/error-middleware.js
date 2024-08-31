0c332e110810f525157020ac161b7f579520b862
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-09-01 01:31:58 +0700

    fix: error middleware

--- src/middleware/error-middleware.js
+++ src/middleware/error-middleware.js
@@ -1,3 +1,4 @@
+import { JoiError } from '../error/joi-error.js';
 import { ResponseError } from '../error/response-error.js';
 
 export const errorMiddleware = async (err, req, res, next) => {
@@ -5,18 +6,35 @@ export const errorMiddleware = async (err, req, res, next) => {
     return next();
   }
 
-  if (err instanceof ResponseError) {
-    res
-      .status(err.status)
-      .json({
-        success: false,
-        errors: err.message,
-      })
-      .end();
-  } else {
-    res.status(500).json({
+  if (err?.type == 'entity.parse.failed' && err?.body) {
+    const error = { message: `Invalid body request`, body: err?.body, path: ['body request'] };
+    return res.status(400).json({
       success: false,
-      errors: err,
+      errors: [error],
+    });
+  } else if (err instanceof ResponseError) {
+    const error = { message: err.message };
+    if (err?.path) {
+      error.path = err.path;
+    }
+    return res.status(err.status).json({
+      success: false,
+      errors: [error],
+    });
+  } else if (err instanceof JoiError) {
+    return res.status(400).json({
+      success: false,
+      errors: JSON.parse(err.message),
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      errors: [
+        {
+          message: 'Internal server error',
+        },
+        err,
+      ],
     });
   }
 };
