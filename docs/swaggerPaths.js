44edf7220addc988a8f142fac5adb8b1ac2476b5
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-09-01 01:29:58 +0700

    docs: login

--- docs/swaggerPaths.js
+++ docs/swaggerPaths.js
@@ -6,10 +6,12 @@ const __dirname = path.dirname(__filename);
 
 // Load individual JSON files
 const registerDoc = JSON.parse(fs.readFileSync(path.join(__dirname, 'paths/register.json'), 'utf-8'));
+const loginDoc = JSON.parse(fs.readFileSync(path.join(__dirname, 'paths/login.json'), 'utf-8'));
 
 // Gather all path
 const docPaths = {
   '/api/v1/auth/register': registerDoc,
+  '/api/v1/auth/login': loginDoc,
 };
 
 // export path
