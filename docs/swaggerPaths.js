3066b18a8edcc15905e2d154dfc37620f82de43c
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-31 21:51:22 +0700

    feat: swagger

---
+++ docs/swaggerPaths.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load individual JSON files
+const registerDoc = JSON.parse(fs.readFileSync(path.join(__dirname, 'paths/register.json'), 'utf-8'));
+
+// Gather all path
+const docPaths = {
+  '/api/v1/auth/register': registerDoc,
+};
+
+// export path
+export default docPaths;
---
+++ src/application/swagger.js
@@ -0,0 +1,38 @@
+import swaggerUi from 'swagger-ui-express';
+import docPaths from '../../docs/swaggerPaths.js';
+
+const port = process.env.PORT | 3000;
+
+const swaggerDocument = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API Documentation',
+    version: '1.0.0',
+    description: 'API Documentation for your project',
+  },
+  servers: [
+    {
+      url: `http://localhost:${port}`,
+      description: 'Development server',
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  paths: { ...docPaths },
+};
+
+export function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
