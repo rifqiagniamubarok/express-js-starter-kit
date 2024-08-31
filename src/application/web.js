a9f9b816374b28fd28d9918b83a9a049b91c6bc9
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-31 21:52:27 +0700

    feat: morgan n swagger

--- src/application/web.js
+++ src/application/web.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import { publicRouter } from '../router/public-router.js';
 import { errorMiddleware } from '../middleware/error-middleware.js';
+import morgan from '../middleware/morgan-middleware.js';
+import { setupSwagger } from './swagger.js';
 
 const web = express();
-web.use(express.json());
 
-web.use(publicRouter);
+// swagger
+setupSwagger(web);
+
+// Middleware
+web.use(express.json());
+web.use(morgan);
+
+web.use('/api/v1', publicRouter);
 
 // Error middlewar
 web.use(errorMiddleware);
