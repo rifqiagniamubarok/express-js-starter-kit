39434bfd62aa6d0e4d8ad7e8e02260d13f6cd4c6
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-09-03 14:24:55 +0700

    feat: cors

--- src/application/web.js
+++ src/application/web.js
@@ -3,6 +3,7 @@ import { errorMiddleware } from '../middleware/error-middleware.js';
 import morgan from '../middleware/morgan-middleware.js';
 import { setupSwagger } from './swagger.js';
 import setupRouter from '../router/index.js';
+import corsMiddleware from '../middleware/cors-middleware.js';
 
 const web = express();
 
@@ -12,6 +13,7 @@ setupSwagger(web);
 // Middleware
 web.use(express.json());
 web.use(morgan);
+web.use(corsMiddleware);
 
 setupRouter(web);
 // Error middlewar
---
+++ src/middleware/cors-middleware.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+
+const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+const corsMiddleware = cors(corsOptions);
+
+export default corsMiddleware;
