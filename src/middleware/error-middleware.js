a2d65d67b40a20c6cb0c0cf4731a77ebaecdc4dc
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-09-06 10:36:03 +0700

    feat: log in every error request

--- src/middleware/error-middleware.js
+++ src/middleware/error-middleware.js
@@ -8,12 +8,14 @@ export const errorMiddleware = (err, _, res, next) => {
   }
 
   if (err?.type === 'entity.parse.failed' && err?.body) {
+    logger.traceFail(JSON.stringify(err));
     const error = { message: `Invalid body request`, body: err?.body, path: ['body request'] };
     return res.status(400).json({
       success: false,
       errors: [error],
     });
   } else if (err instanceof ResponseError) {
+    logger.traceFail(JSON.stringify(err));
     const error = { message: err.message };
     if (err?.path) {
       error.path = err.path;
@@ -23,6 +25,7 @@ export const errorMiddleware = (err, _, res, next) => {
       errors: [error],
     });
   } else if (err instanceof JoiError) {
+    logger.traceFail(JSON.stringify(err));
     return res.status(400).json({
       success: false,
       errors: JSON.parse(err.message),
